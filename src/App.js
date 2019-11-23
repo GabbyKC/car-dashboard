dbe5be88dbf81b1da3968a036a7e9e3c6f800ec7
Gabriella-Kate <gabriellakate04@gmail.com>
2019-11-23 19:58:04 +0100

    basic structure setup + data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import data from './data';
 
-function App() {
-  return (
-    <div>
-      <h1>test</h1>
-    </div>
-  );
+class App extends Component {
+    renderTableData() {
+        return data.map((car, index) => {
+            const {name, deviceID, HWKey, charge, mileage, GPS} = car;
+            return (
+                <tr key={index} className='car-info'>
+                    <td>{name}</td>
+                    <td>{deviceID}</td>
+                    <td>{HWKey}</td>
+                    <td>{charge}%</td>
+                    <td>{mileage}</td>
+                    <td>{GPS}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className='dashboard-wrapper'>
+                <div className='header'>
+                    <div className='brand-name'>Device Simulator Dashboard</div>
+                    <div>add goes here</div>
+                </div>
+
+                <table>
+                    <thead>
+                    <tr className='table-headings'>
+                        <th>NAME</th>
+                        <th>DEVICE ID</th>
+                        <th>HW KEY</th>
+                        <th>CHARGE</th>
+                        <th>MILEAGE</th>
+                        <th>GPS</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderTableData()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default App;

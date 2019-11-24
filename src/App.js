6a412a0ec3d70860cdbb4e30b1af86f7fd2568ed
Gabriella-Kate <gabriellakate04@gmail.com>
2019-11-25 00:29:14 +0100

    edit functionality + ui for edit screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPencilAlt, faTrash, faPlus} from "@fortawesome/free-solid-svg-icons";
 import DeviceView from "./DeviceView";
 import './App.css';
-// import data from './data';
 
 class App extends Component {
     state = {
@@ -15,7 +14,7 @@ class App extends Component {
                 "HWKey": "98765432",
                 "charge": 80,
                 "mileage": 4000,
-                "distance": 23,
+                "distance": 12.3,
                 "immobilizer": true,
                 "keyInHolder": true,
                 "doors": true,
@@ -24,6 +23,7 @@ class App extends Component {
                 "GPS": "On",
                 "ignition": true,
                 "LTE": false,
+                "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.636431985886!2d13.41102065166224!3d52.52191837971555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e1edb11286f%3A0x30fc01c366e4166e!2sAlexanderplatz!5e0!3m2!1sen!2sde!4v1574620427024!5m2!1sen!2sde"
             },
             {
                 "name": 2,
@@ -40,6 +40,7 @@ class App extends Component {
                 "GPS": "Off",
                 "ignition": true,
                 "LTE": false,
+                "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.9483064299807!2d13.375510051662115!3d52.516274579714484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851c655f20989%3A0x26bbfb4e84674c63!2sBrandenburg%20Gate!5e0!3m2!1sen!2sde!4v1574626587136!5m2!1sen!2sde"
             },
             {
                 "name": 3,
@@ -56,6 +57,7 @@ class App extends Component {
                 "GPS": "Off",
                 "ignition": true,
                 "LTE": false,
+                "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.636431985886!2d13.41102065166224!3d52.52191837971555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e1edb11286f%3A0x30fc01c366e4166e!2sAlexanderplatz!5e0!3m2!1sen!2sde!4v1574620427024!5m2!1sen!2sde"
             },
             {
                 "name": 4,
@@ -72,13 +74,32 @@ class App extends Component {
                 "GPS": "On",
                 "ignition": true,
                 "LTE": false,
+                "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.9483064299807!2d13.375510051662115!3d52.516274579714484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851c655f20989%3A0x26bbfb4e84674c63!2sBrandenburg%20Gate!5e0!3m2!1sen!2sde!4v1574626587136!5m2!1sen!2sde"
             }
         ]
     };
 
-    noLongerEditing = () => {
-        this.setState({...this.state, currentlyEditing: null});
+    setCurrentlyEditing = (currentlyEditing) => {
+        this.setState({...this.state, currentlyEditing: currentlyEditing});
     };
+
+    deleteDevice = () => {
+        console.log('Delete not implemented yet')
+    };
+
+    updateCar = (carName, e) => {
+        const {name, value, type, checked} = e.target;
+
+        let updatedCars = this.state.cars.map(car => (
+            car.name === carName ? {...car, [name]: (type === 'checkbox' ? checked : value)} : car
+        ));
+
+        this.setState({
+            ...this.state,
+            cars: updatedCars
+        })
+    };
+
 
     renderTableData() {
         return this.state.cars.map((car, index) => {
@@ -93,26 +114,40 @@ class App extends Component {
                     <td>{GPS}</td>
                     <td>{mileage}</td>
                     <td className='center' onClick={() => {
-                        this.setState({...this.state, currentlyEditing: car})
-                    }}><FontAwesomeIcon className='table-icon' icon={faPencilAlt}/> <span className='update-buttons'>Edit</span></td>
-                    <td className='center'><FontAwesomeIcon className='table-icon' icon={faTrash}/> <span className='update-buttons'>Delete</span></td>
+                        this.setCurrentlyEditing(car.name)
+                    }}>
+                        <FontAwesomeIcon className='table-icon' icon={faPencilAlt}/>
+                        <span className='update-buttons'>Edit</span>
+                    </td>
+                    <td onClick={this.deleteDevice} className='center'>
+                        <FontAwesomeIcon className='table-icon' icon={faTrash}/>
+                        <span className='update-buttons'>Delete</span>
+                    </td>
                 </tr>
             )
         })
     }
 
+    getCarByName = (name) => {
+        return this.state.cars.find(car => {
+            return car.name === name;
+        })
+    };
+
     render() {
         const currentlyEditing = this.state.currentlyEditing;
 
         if (currentlyEditing) {
+            const car = this.getCarByName(currentlyEditing);
             return (
-                <DeviceView car={this.state.currentlyEditing} goBack={this.noLongerEditing}/>
+                <DeviceView car={car} goBack={() => this.setCurrentlyEditing(null)} update={this.updateCar}/>
             )
         }
         return (
             <div className='dashboard-wrapper'>
                 <div className='header'>
-                    <div className='brand-name'>Device Simulator Dashboard</div> {/* dummy icon, no actual functionality */}
+                    <div className='brand-name'>Device Simulator Dashboard</div>
+                    {/* dummy icon, no actual functionality */}
                     <div className='add-device'><FontAwesomeIcon icon={faPlus}/> Add Device</div>
                 </div>
 
--- src/DeviceView.js
+++ src/DeviceView.js
@@ -4,28 +4,111 @@ import {faChevronCircleLeft} from "@fortawesome/free-solid-svg-icons";
 
 class DeviceView extends Component {
     render() {
+        const {car} = this.props;
+
         return (
             <section className='dashboard-wrapper device-view'>
-                <div onClick={() => this.props.goBack()}><FontAwesomeIcon className='back-arrow' icon={faChevronCircleLeft}/></div>
+                <div onClick={() =>  {this.props.goBack()}}>
+                    <FontAwesomeIcon className='back-arrow' icon={faChevronCircleLeft}/>
+                </div>
                 <div className='left-menu'>
-                    <p className='device-name'>Device {this.props.car.name}</p>
+                    <p className='device-name'>Device {car.name}</p>
                     <p>Device ID
-                        <span className='device-content'>{this.props.car.deviceID}</span>
+                        <span className='device-content'>{car.deviceID}</span>
                     </p>
                     <p>HW Key
-                        <span className='device-content'>{this.props.car.HWKey}</span>
+                        <span className='device-content'>{car.HWKey}</span>
                     </p>
 
                     <div className='toggles-container'>
+                        <div className='toggle'>
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Immobilizer</div>
+                                <label className="switch">
+                                    <input name="immobilizer" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.immobilizer}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
 
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Doors</div>
+                                <label className="switch">
+                                    <input name="doors" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.doors}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Handbrake</div>
+                                <label className="switch">
+                                    <input name="handbrake" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.handbrake}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Ignition</div>
+                                <label className="switch">
+                                    <input name="ignition" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.ignition}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className='toggle'>
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Key In Holder</div>
+                                <label className="switch">
+                                    <input name="keyInHolder" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.keyInHolder}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>Fuelcard</div>
+                                <label className="switch">
+                                    <input name="fuelCard" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.fuelCard}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>GPS</div>
+                                <label className="switch">
+                                    <input name="GPS" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.GPS}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+
+                            <div className='toggle-wrapper'>
+                                <div className='toggle-name'>LTE</div>
+                                <label className="switch">
+                                    <input name="LTE" type="checkbox" onChange={(e) => this.props.update(car.name, e)} checked={car.LTE}/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
                 <div className='right-menu'>
-                    <div>hi</div>
-                    <div>bye</div>
-                </div>
+                    <div className='map-container'>
+                        <div className='location-heading'>Location</div>
+                        <iframe title="Device Location" src={car.location} frameBorder="0"></iframe>
+                    </div>
 
+                    <div className='right-menu-bottom'>
+                        <div className='bottom-menu-item'><span className='bottom-heading'>Charge</span>
+                            <input name="charge" type="range" min="0" max="100" onChange={(e) => this.props.update(car.name, e)} value={car.charge}/> {car.charge}%
+                        </div>
+                        <div className='bottom-menu-item'><span className='bottom-heading'>Mileage</span>
+                            <input name="mileage" type="text" onChange={(e) => this.props.update(car.name, e)} value={car.mileage}/> km
+                        </div>
+                        <div className='bottom-menu-item'><span className='bottom-heading'>Distance</span>
+                            <input name="distance" type="number" onChange={(e) => this.props.update(car.name, e)} value={car.distance}/> km
+                        </div>
+                    </div>
+                </div>
             </section>
         );
     }
